df375361c52d3017d35fda3ded245da55601b754
Mia Altieri <mgaltier200@gmail.com>
2026-05-06 11:58:32 -0700

    aws cdk basics

--- stratum-infra/stratum_infra/stratum_infra_stack.py
+++ stratum-infra/stratum_infra/stratum_infra_stack.py
@@ -5,12 +5,15 @@ from aws_cdk import (
 )
 from constructs import Construct
 
+
 class StratumInfraStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        # This is where we will:
+        #  create the S3 bucket and its associated policies and lifecycle rules
 
         # example resource
         # queue = sqs.Queue(
